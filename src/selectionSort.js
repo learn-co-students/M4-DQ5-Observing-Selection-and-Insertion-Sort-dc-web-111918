b38c1ddfec8447121f960441114708f69c047c2c
github username <github email address>
2019-02-05 22:39:26 -0500

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,19 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let i 
+  let j 
+  let n = arr.length
+  
+  for(j=0; j < n-1; j++){
+    let iMin = j
+    for(i=j+1; i < n; i++) {
+      if (arr[i] < arr[iMin]) {
+        iMin = i
+      }
+    let temp = arr[j]
+    arr[j] = arr[iMin]
+    arr[iMin] = temp
+    }
+  }
 }
